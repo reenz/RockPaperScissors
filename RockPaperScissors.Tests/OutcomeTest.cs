27913309de0638aa488b819d9c03340bb692ab00
Reena <reena.sanjay.sharma@gmail.com>
2018-05-22 22:39:07 +0100

    added test for returning player two  wins

--- RockPaperScissors.Tests/OutcomeTest.cs
+++ RockPaperScissors.Tests/OutcomeTest.cs
@@ -9,13 +9,23 @@ namespace RPS.Lib.Tests
     {
 
         [Test]
-        public void ShouldReturnWinner()
+        public void ShouldReturnPlayerOneWinner()
         {   
             var playerOneScore = 2;
             var playerTwoScore = 1;
             var outcome= new Outcome(playerOneScore, playerTwoScore);
             var result = outcome.Winner();
             Assert.AreEqual(result,"Player One Wins");
+        }
+
+        [Test]
+        public void ShouldReturnPlayerTwoWinner()
+        {   
+            var playerOneScore = 1;
+            var playerTwoScore = 2;
+            var outcome= new Outcome(playerOneScore, playerTwoScore);
+            var result = outcome.Winner();
+            Assert.AreEqual(result,"Player Two Wins");
         }
     }
 }
